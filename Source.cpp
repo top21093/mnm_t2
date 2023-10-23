6d0997346c5c274c7d2da812d29f7f9f0a6dac88
cuongcachep <cuongnguyen0834@gmail.com>
2023-10-23 14:34:58 +0700

    a

--- Source.cpp
+++ Source.cpp
@@ -2,12 +2,21 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b) 
+	{
+		return a - b;
+	} 
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	int a, b;
+	cout << "\n Nhap a : ";
+	cin >> a;
+	cout << "\n Nhap b : ";
+	cin >> b;
+	cout << hieu;
 	system("pause");
 	return 0;
 }
