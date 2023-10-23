4c0cc4697dcbe8d668dc7da56b62147fe2269e72
Thuan <mthuan1004@gmail.com>
2023-10-23 14:28:43 +0700

    a

--- Source.cpp
+++ Source.cpp
@@ -3,11 +3,24 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
-float thuong(int a, int b);
+int tich(int a, int b) {
+	return a * b;
+}
+float thuong(int a, int b) {
+	if (b == 0) {
+		return new throw("Suc vat bi loi roi");
+	}
+	else {
+		return (float)a / b;
+	}
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	int tich = tich(3, 5);
+	int thuong = thuong(3, 0);
+	cout << tich;
+	cout << thuong;
 	system("pause");
 	return 0;
 }
